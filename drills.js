b1a69a60518fa8c551189ce34c463c57156b59f5
Sara R Mills <SaraRMills@gmail.com>
2020-02-10 16:09:00 -0500

    created second function for year of birth calculation

--- drills.js
+++ drills.js
@@ -1,7 +1,11 @@
-function createGreeting(name, age){
-let yearOfBirth = 2020 - age;
+function getYearOfBirth(age){
+    return 2020 - age;
+}
 
-return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${yearOfBirth}`
+function createGreeting(name, age){
+    const yob = getYearOfBirth(age);
+
+    return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${yob}.`
 }
 const greeting1 = createGreeting('Sara', 32);
 console.log(greeting1);
