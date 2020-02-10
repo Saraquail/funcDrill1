f7e24b4613057202c0e40b9823bab2a2669d2311
Sara R Mills <SaraRMills@gmail.com>
2020-02-10 16:46:59 -0500

    added several parameter checks and error messages

--- drills.js
+++ drills.js
@@ -1,9 +1,15 @@
+
 function getYearOfBirth(age){
     return 2020 - age;
 }
 
 function createGreeting(name, age){
-
+    if(typeof age !== "number" || typeof name !== "string"){
+        throw new TypeError("Type Error")
+    }
+    if (name === undefined || age === undefined){
+        throw new Error ("Arguments not valid"); 
+    }
     if(age < 0){
         throw new Error ("Age cannot be negative"); 
     }
@@ -13,7 +19,7 @@ function createGreeting(name, age){
 return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${yob}.`
 }
 try {
-    const greeting1 = createGreeting('Sara', 32);
+    const greeting1 = createGreeting(44, 32);
     console.log(greeting1);
 } 
 
