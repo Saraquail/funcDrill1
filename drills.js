8b001a422e6e5cd1351e34e3d54ed3c6df6fcf61
Sara R Mills <SaraRMills@gmail.com>
2020-02-10 16:37:41 -0500

    added try and catch blocks

--- drills.js
+++ drills.js
@@ -3,9 +3,20 @@ function getYearOfBirth(age){
 }
 
 function createGreeting(name, age){
-    const yob = getYearOfBirth(age);
 
-    return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${yob}.`
+    if(age < 0){
+        throw new Error ("Age cannot be negative"); 
+    }
+    const yob = getYearOfBirth(age);
+   
+    
+return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${yob}.`
 }
-const greeting1 = createGreeting('Sara', 32);
-console.log(greeting1);
+try {
+    const greeting1 = createGreeting('Sara', 32);
+    console.log(greeting1);
+} 
+
+catch (e) {
+    console.error(e.message);
+}
